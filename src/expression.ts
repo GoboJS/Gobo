a422c2236612bbb0dd5832abbe4deb4d8de23223
Nycto <James@RoundEights.com>
2015-02-25 08:40:22 -0800

    Support quoted expressions

--- src/expression.ts
+++ src/expression.ts
@@ -2,6 +2,28 @@
 
 module Expr {
 
+    // Splits a string by unquoted characters
+    // @see http://stackoverflow.com/questions/9609973
+    var split = {
+        '.': /(?:(["'])(?:\\.|[^\1])*?\1|\\.|[^\.])+/g
+    };
+
+    /** Strips quotes from the values in an array of string */
+    function stripQuotes( values: string[] ) {
+        return values.map(value => {
+            var char0 = value.charAt(0);
+            if (
+                (char0 === "'" || char0 === '"') &&
+                char0 === value.charAt(value.length - 1)
+            ) {
+                return value.substr(1, value.length - 2);
+            }
+            else {
+                return value;
+            }
+        });
+    }
+
     /** A parsed expression */
     export class Expression {
 
@@ -9,7 +31,7 @@ module Expr {
         public keypath: string[]
 
         constructor( expr: string ) {
-            this.keypath = expr.split(".");
+            this.keypath = stripQuotes(expr.match(split['.']));
         }
 
         /** Returns the value of this expression */
